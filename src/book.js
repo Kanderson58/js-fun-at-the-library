028a0dd648ef8fa14e4e7c353f3e4918ba476178
Kara Anderson <kanderson8585@gmail.com>
2022-12-01 11:54:26 -0700

    Save progress on book functions

--- src/book.js
+++ src/book.js
@@ -1,10 +1,18 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return (`The ${title}`);
+}
+
+function buildMainCharacter (name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
