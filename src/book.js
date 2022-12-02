7c7f305315ebcb7603a60dacf8954648c1ba58af
Kara Anderson <kanderson8585@gmail.com>
2022-12-01 18:07:09 -0700

    Complete book iteration all tests passed

--- src/book.js
+++ src/book.js
@@ -10,11 +10,36 @@ function buildMainCharacter (name, age, pronouns) {
   }
 }
 
+
+function saveReview (newReview, emptyArray) {
+  if (emptyArray.includes(newReview) === false) {
+  emptyArray.push(newReview);
+  }
+}
+
+function calculatePageCount (title) {
+  return title.length * 20
+}
+
+
+function writeBook (title, mainCharacter, genre) {
+  return book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    genre: genre,
+    pageCount: calculatePageCount(title)
+  }
+}
+
+function editBook (title) {
+  return title.pageCount *= .75
+}
+
 module.exports = {
   createTitle,
   buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
