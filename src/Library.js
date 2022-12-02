e14ee3cdf8fa44ad22b8ef3c2d51584931ce8d70
Kara Anderson <kanderson8585@gmail.com>
2022-12-02 16:52:45 -0700

    Save first draft of Library

--- src/Library.js
+++ src/Library.js
@@ -12,9 +12,19 @@ function addBook (libraryName, bookTitle) {
   return libraryName.shelves[bookTitle.genre].push(bookTitle)
 }
 
-function checkoutBook () {
+function checkoutBook (libraryName, bookTitle, bookGenre) {
+  var accessObject = libraryName.shelves[bookGenre]
+  for (i = 0; i < accessObject.length; i ++) {
+    if (accessObject[i].title === bookTitle) {
+      accessObject.splice(accessObject.indexOf(bookTitle, 1))
+      return `You have now checked out ${bookTitle} from the ${libraryName.name}`
+    }
+  }
+    return `Sorry, there are currently no copies of ${bookTitle} available at the ${libraryName.name}`
   
 }
+
+
 
 module.exports = {
   createLibrary,
