5fb799d35447e8e9fdef036cbc36d6e60f68311f
Kara Anderson <kanderson8585@gmail.com>
2022-12-03 13:50:21 -0700

    Save work on librarian

--- src/Library.js
+++ src/Library.js
@@ -5,22 +5,22 @@ function createLibrary (libraryName) {
       fiction: [], 
       nonFiction: []}
   }
-  return library
+  return library;
 }
 
 function addBook (libraryName, bookTitle) {
-  return libraryName.shelves[bookTitle.genre].push(bookTitle)
+  libraryName.shelves[bookTitle.genre].push(bookTitle);
 }
 
 function checkoutBook (libraryName, bookTitle, bookGenre) {
-  var accessObject = libraryName.shelves[bookGenre]
+  var accessObject = libraryName.shelves[bookGenre];
   for (i = 0; i < accessObject.length; i ++) {
     if (accessObject[i].title === bookTitle) {
-      accessObject.splice(accessObject.indexOf(bookTitle, 1))
-      return `You have now checked out ${bookTitle} from the ${libraryName.name}`
+      accessObject.splice(accessObject.indexOf(bookTitle, 1));
+      return `You have now checked out ${bookTitle} from the ${libraryName.name}`;
     }
   }
-    return `Sorry, there are currently no copies of ${bookTitle} available at the ${libraryName.name}`
+    return `Sorry, there are currently no copies of ${bookTitle} available at the ${libraryName.name}`;
   
 }
 
--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,27 @@
+const { addBook } = require("./Library")
+
 class Librarian {
-  constructor() {
-    
+  constructor(librarianName, worksAt, patronName, bookTitle, bookGenre) {
+    this.name = librarianName
+    this.library = worksAt
+    this.book = bookTitle
+    this.genre = bookGenre
+  }
+  greetPatron(patronName, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${patronName}!`
+    } else {
+      return `Hello, ${patronName}!`
+    }
+  }
+  findBook(bookTitle) {
+    if (this.book === bookTitle) {
+      return `Yes, we have ${bookTitle}`
+    // } else {
+    //   return `Sorry, we do not have ${bookTitle}`
+    }
   }
 }
+
 
 module.exports = Librarian;
