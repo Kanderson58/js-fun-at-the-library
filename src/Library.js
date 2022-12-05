125526a3aa8551691715b614e7fb0623960e8d8b
Kara Anderson <kanderson8585@gmail.com>
2022-12-04 19:33:46 -0700

    Clean up syntax and finalize project

--- src/Library.js
+++ src/Library.js
@@ -16,15 +16,13 @@ function checkoutBook (libraryName, bookTitle, bookGenre) {
   var accessObject = libraryName.shelves[bookGenre];
   for (i = 0; i < accessObject.length; i ++) {
     if (accessObject[i].title === bookTitle) {
-      accessObject.splice(accessObject.indexOf(bookTitle), 1);
+      accessObject.splice(i, 1);
       return `You have now checked out ${bookTitle} from the ${libraryName.name}`;
     }
   }
     return `Sorry, there are currently no copies of ${bookTitle} available at the ${libraryName.name}`;
   
 }
-
-
 
 module.exports = {
   createLibrary,
--- src/librarian.js
+++ src/librarian.js
@@ -2,46 +2,29 @@ const { addBook } = require("./Library")
 
 class Librarian {
   constructor(librarianName, worksAt) {
-    this.name = librarianName
-    this.library = worksAt
+    this.name = librarianName;
+    this.library = worksAt;
   }
   greetPatron(patronName, isMorning) {
     if (isMorning === true) {
-      return `Good morning, ${patronName}!`
+      return `Good morning, ${patronName}!`;
     } else {
-      return `Hello, ${patronName}!`
+      return `Hello, ${patronName}!`;
     }
   }
-  // function addBook (libraryName, bookTitle) {
-  //   libraryName.shelves[bookTitle.genre].push(bookTitle);
-  // }
-
-
-  // function checkoutBook (libraryName, bookTitle, bookGenre) {
-  //   var accessObject = libraryName.shelves[bookGenre];
-  //   for (i = 0; i < accessObject.length; i ++) {
-  //     if (accessObject[i].title === bookTitle) {
-  //       accessObject.splice(accessObject.indexOf(bookTitle), 1);
-  //       return `You have now checked out ${bookTitle} from the ${libraryName.name}`;
-  //     }
-  //   }
-  //     return `Sorry, there are currently no copies of ${bookTitle} available at the ${libraryName.name}`;
-    
-  // }
-
 
   findBook(bookTitle) {
-    var shelfGenres = Object.values(this.library.shelves)
+    var shelfGenres = Object.values(this.library.shelves);
     for (var i = 0; i < shelfGenres.length; i++) {
-      var theTitle = shelfGenres[i][i].title
+      var theTitle = shelfGenres[i][i].title;
       if (theTitle === bookTitle) {
         for (var j = 0; j < theTitle.length; j++) {
-          var theTitleArray = shelfGenres[i]
-          theTitleArray.splice(theTitle.indexOf(bookTitle), 1);
+          var theTitleArray = shelfGenres[i];
+          theTitleArray.splice(j, 1);
           return `Yes, we have ${bookTitle}`;
         }
       } else {
-        return `Sorry, we do not have ${bookTitle}`
+        return `Sorry, we do not have ${bookTitle}`;
       }
     }
   }
@@ -51,6 +34,5 @@ class Librarian {
   }
 
 }
-
 
 module.exports = Librarian;
